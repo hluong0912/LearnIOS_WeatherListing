fe15fa28ea211a4440a9490acaacf04851649e3d
hluong0912 <huy.luong0912@gmail.com>
2022-06-04 21:06:57 -0500

    Add Views on mainstoryboard and add basic controller classes for those views

---
+++ WeatherListing/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddWeatherCityViewController.swift
+//  WeatherListing
+//
+//  Created by Huy on 6/4/22.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController{
+    
+    
+    
+}
---
+++ WeatherListing/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherListTableViewController.swift
+//  WeatherListing
+//
+//  Created by Huy on 6/4/22.
+//
+
+import Foundation
+import UIKit
+
+
+class WeatherListTableViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+}
